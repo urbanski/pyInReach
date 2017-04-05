574bee17e9e9df2368ee01a9e5845d8e80a1b18e
will urbanski <will.urbanski@gmail.com>
2017-04-04 19:17:50 -0600

    add software license

--- pyinreach.py
+++ pyinreach.py
@@ -1,13 +1,32 @@
-import requests
+# MIT License
 
-#todo-- discover deviceids
+# Copyright (c) 2017 Will Urbanski
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
 
 def SendMessageToDevices(mapshare_url, message):
 	h = {}
 	h['X-Requested-With'] = 'XMLHttpRequest'
 	h['Accept'] = 'application/json, text/javascript, */*;'
 	h['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
-	h['User-Agent'] = 'Mozilla/5.0'
 
 	req = requests.post('%s/Map/SendMessageToDevices' % mapshare_url, headers=h, allow_redirects=False, data = message.to_dict())
 
